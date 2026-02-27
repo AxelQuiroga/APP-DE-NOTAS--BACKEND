316c00d9eef216300a5980a0d7618060d60c1dea
Seby <lokuraa18@hotmail.com>
2026-02-27 13:57:17 -0300

    cors acceso  frontend

--- src/server.js
+++ src/server.js
@@ -8,7 +8,8 @@ dotenv.config()
 const app = express()
 
 app.use(cors({
-    origin: "http://localhost:5173"
+    origin: ["http://localhost:5173","https://guileless-alpaca-c1ab24.netlify.app/"]
+    
 }))
 
 app.use(express.json())
